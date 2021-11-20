aa37597c3900fefdc0ff63c6c62a32eef22e9e26
rt160918@gmail.com <rt160918@gmail.com>
2021-11-20 20:29:52 +0530

    Enter

---
+++ games/pong.py
@@ -0,0 +1,97 @@
+import turtle
+
+win = turtle.Screen()
+win.title("pong by @thakur_sahab.999")
+win.bgcolor("black")
+win.setup(width=800, height=600)
+win.tracer(0)
+
+#paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350,0)
+#paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350,0)
+
+#ball
+
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("blue")
+ball.penup()
+ball.setx(-390)
+ball.dx = 0.1
+ball.dy = 0.1
+
+
+#functions
+
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+#keyboard binding
+win.listen()
+win.onkeypress(paddle_a_up, "w")
+win.onkeypress(paddle_a_down, "s")
+win.onkeypress(paddle_b_up, "Up")
+win.onkeypress(paddle_b_down, "Down")
+
+#main game loop
+while True:
+    win.update()
+
+    #move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    #border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.setx(390)
+        ball.dx *= -1
+
+    if ball.xcor() < -390:
+        ball.setx(-390)
+        ball.dx *= -1
+
+    #ball paddle colision
+    if ball.xcor()> 345 and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if ball.ycor()> 345 and (ball.xcor() < paddle_a.xcor() + 50 and ball.xcor() > paddle_a.xcor() - 50):
+        ball.dx *= -1
---
+++ learning/numbers.py
@@ -0,0 +1,12 @@
+num = -5
+#print(str(num) + ' is my favourite no')
+
+# absolute; print(abs(num))
+
+#power of a number; print(pow(4,5))
+
+# maximum number; print(max(56,78))
+
+#minimum number; print(min(67,9))
+
+#rounds of number print(round(3.456))
